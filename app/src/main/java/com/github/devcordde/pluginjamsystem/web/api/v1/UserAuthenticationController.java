337a063df555aeae23c61be8314b1fb5bf896398
Yannick Lamprecht <yannicklamprecht@live.de>
2022-04-15 15:48:42 +0200

    dirty implementation for frontend user handle

--- app/src/main/java/com/github/devcordde/pluginjamsystem/web/api/v1/UserAuthenticationController.java
+++ app/src/main/java/com/github/devcordde/pluginjamsystem/web/api/v1/UserAuthenticationController.java
@@ -1,8 +1,5 @@
 package com.github.devcordde.pluginjamsystem.web.api.v1;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,21 +11,12 @@ public class UserAuthenticationController {
 
 
 
-    private final OAuth2AuthorizedClientService authorizedClientService;
-
-    @Autowired
-    public UserAuthenticationController(OAuth2AuthorizedClientService authorizedClientService) {
-        this.authorizedClientService = authorizedClientService;
-    }
 
 
 
     @GetMapping
     public String getLoginInfo(OAuth2AuthenticationToken authentication) {
-        OAuth2AuthorizedClient client = authorizedClientService
-                .loadAuthorizedClient(
-                        authentication.getAuthorizedClientRegistrationId(),
-                        authentication.getName());
+
 
         return "Login accomplished. Hello " + authentication.getPrincipal().getAttributes().get("username") + ""
                 + "See implementation at github.com/Samurus/spring-boot-discord-oauth-example";
--- app/src/main/java/com/github/devcordde/pluginjamsystem/web/api/v1/UserDataController.java
+++ app/src/main/java/com/github/devcordde/pluginjamsystem/web/api/v1/UserDataController.java
@@ -2,6 +2,8 @@ package com.github.devcordde.pluginjamsystem.web.api.v1;
 
 
 import com.github.devcordde.pluginjamsystem.dto.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
@@ -15,12 +17,36 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/user")
 public class UserDataController {
 
+    private static final Logger log = LoggerFactory.getLogger(UserDataController.class);
+
+
+    private final OAuth2AuthorizedClientService authorizedClientService;
+
+    @Autowired
+    public UserDataController(OAuth2AuthorizedClientService authorizedClientService) {
+        this.authorizedClientService = authorizedClientService;
+    }
+
+
     @GetMapping(path = "/current")
-    public ResponseEntity<User> getLoggedInUser() {
+    public ResponseEntity<User> getLoggedInUser(OAuth2AuthenticationToken authentication) {
+
+
+        OAuth2AuthorizedClient client = authorizedClientService
+                .loadAuthorizedClient(
+                        authentication.getAuthorizedClientRegistrationId(),
+                        authentication.getName());
+
+
+        var attributes = authentication.getPrincipal().getAttributes();
+        attributes.forEach((s, o) -> {
+            log.info("name: {}, value: {}", s, o);
+        });
+
         return ResponseEntity.ok(new User(
-                "ysl#98999",
-                "Otto",
-                "https://randomuser.me/api/portraits/women/85.jpg"
+                String.format("%s#%s", attributes.get("username"), attributes.get("discriminator")),
+                (String) attributes.get("username"),
+                String.format("https://cdn.discordapp.com/avatars/%s/%s.png", attributes.get("id"), attributes.get("avatar"))
         ));
     }
 }

3b0142c13d613010d087ea94e2ee7f552cbe87af
Florian Fülling <46890129+RainbowDashLabs@users.noreply.github.com>
2022-04-15 18:42:17 +0200

    Play around with painful centering

--- app/src/main/resources/static/js/pages/Welcome.js
+++ app/src/main/resources/static/js/pages/Welcome.js
@@ -5,50 +5,66 @@ import Anon from "../components/menu/Anon.js";
 
 
 export default {
-	template: `
+    template: `
 			<v-app>
 			<template v-if="user != null">
-						  <v-navigation-drawer>
-							<User
-							:user="user"
-							></User>
-							<Anon
-							v-else
-							></Anon>
-							<Menu
-							></Menu>
-					  </v-navigation-drawer>
-					  <v-main>
-						    <v-container fluid>
-						    </v-container>
-					  </v-main>
+                  <v-navigation-drawer>
+                    <User
+                    :user="user"
+                    ></User>
+                    <Anon
+                    v-else
+                    ></Anon>
+                    <Menu
+                    ></Menu>
+              </v-navigation-drawer>
+              <v-main>
+                    <v-container fluid>
+                    </v-container>
+              </v-main>
 			  <v-footer app>
 			  </v-footer>
 			</template>
 			<template v-else>
-				<Anon></Anon>
-			</template>
+                <v-container justify="space-around" fill-height fluid>
+                      <v-sheet
+                      color="dark-grey"
+                          elevation="5"
+                          class="mx-auto"
+                          height="500"
+                          width="400"
+                        >    
+                            <v-row class="flex-grow-1">
+                                <v-col cols="12" class="pa-10 text-center">
+                                    Login
+                                </v-col>
+                            </v-row>
+                            <v-row class="flex-grow-1">
+                            Image
+                            </v-row>
+                            <v-row class="flex-grow-1">
+                            <Anon></Anon>
+                            </v-row>
+                        </v-sheet>
+                </v-container>
+            </template>
 		
 	</v-app>
 `,
-	data() {
-		return {
-			user_service: user_service,
-			user: null,
-		}
-	},
-	components: {
-		Anon: Anon,
-		User: User,
-		Menu: Menu,
-	},
-	mounted() {
-		this.user = user;
-	},
+    data() {
+        return {
+            user_service: user_service,
+            user: null,
+        }
+    },
+    components: {
+        Anon: Anon,
+        User: User,
+        Menu: Menu,
+    },
+    mounted() {
+        this.user = user;
+    },
 
-	methods: {}
+    methods: {}
 }
-
-
-
-

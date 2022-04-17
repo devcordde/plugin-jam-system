d3f3ce1d601bf6faef69e10556eb45bc1f2a91a4
Yannick Lamprecht <yannicklamprecht@live.de>
2022-04-17 17:10:29 +0200

    source formatting

--- app/src/main/resources/static/js/components/main/TeamList.js
+++ app/src/main/resources/static/js/components/main/TeamList.js
@@ -4,29 +4,25 @@ import CollapsedTeam from "./teamlist/CollapsedTeam.js";
 
 export default {
 	template: `
-	<v-card
-    class="mx-auto"
-  >
-   <v-list>
-      <v-list-group
-        v-for="team in teams"
-        :key="team.profile.name"
-        v-model="team.active"
-        no-action
-      >
-        <template
-          v-slot:activator
-        >
-	        <Collapsed-team
-	          :profile="team.profile"
-	        ></Collapsed-team>
-				</template>
-				<Team-profile
-				:team="team"
-				></Team-profile>
-      </v-list-group>
-    </v-list>
-  </v-card>
+			<v-card class="mx-auto">
+				   <v-list>
+					      <v-list-group
+					        v-for="team in teams"
+					        :key="team.profile.name"
+					        v-model="team.active"
+					        no-action
+					      >
+						        <template v-slot:activator >
+								        <Collapsed-team
+								          :profile="team.profile"
+								        ></Collapsed-team>
+										</template>
+										<Team-profile
+										:team="team"
+										></Team-profile>
+					      </v-list-group>
+				   </v-list>
+		  </v-card>
 	`,
 	components: {
 		TeamProfile: TeamProfile,

88b4191ae42aedd9d0e98784d767ab22ec63bcf6
Yannick Lamprecht <yannicklamprecht@live.de>
2022-04-27 23:32:21 +0200

    Finish up the whole thing, concept of local mode dropped

--- app/src/main/java/com/github/devcordde/pluginjamsystem/resolver/user/DiscordUserResolver.java
+++ app/src/main/java/com/github/devcordde/pluginjamsystem/resolver/user/DiscordUserResolver.java
@@ -3,7 +3,7 @@ package com.github.devcordde.pluginjamsystem.resolver.user;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.devcordde.pluginjamsystem.dto.User;
-import com.github.devcordde.pluginjamsystem.services.UsersService;
+import com.github.devcordde.pluginjamsystem.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Component;
@@ -14,11 +14,11 @@ public class DiscordUserResolver implements UserResolver {
     private final ObjectMapper mapper = new ObjectMapper()
             .configure(DeserializationFeature.FAIL_ON_MISSING_CREATOR_PROPERTIES, false)
             .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-    private final UsersService usersService;
+    private final UserService userService;
 
     @Autowired
-    public DiscordUserResolver(UsersService usersService) {
-        this.usersService = usersService;
+    public DiscordUserResolver(UserService userService) {
+        this.userService = userService;
     }
 
     @Override
@@ -27,7 +27,7 @@ public class DiscordUserResolver implements UserResolver {
 
         var user = mapper.convertValue(attributes, User.class);
 
-        var mutualGuilds = usersService.getMutualGuilds(user.id());
+        var mutualGuilds = userService.getMutualGuilds(user.id());
         user.guilds(mutualGuilds);
         if (!mutualGuilds.isEmpty()) {
             user.currentGuild(mutualGuilds.get(0));

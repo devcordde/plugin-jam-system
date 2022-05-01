8dde40a0fc2b4b2b4af21d8aa9cc52f5df02249e
Yannick Lamprecht <yannicklamprecht@live.de>
2022-05-01 23:03:39 +0200

    Added namespace to setup, ForwardedHeaderFilter

--- app/src/main/java/com/github/devcordde/pluginjamsystem/web/pages/IndexPageController.java
+++ app/src/main/java/com/github/devcordde/pluginjamsystem/web/pages/IndexPageController.java
@@ -6,11 +6,14 @@ import com.github.devcordde.pluginjamsystem.resolver.UserInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientProperties;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -18,31 +21,25 @@ import java.util.stream.Collectors;
 @Controller
 public class IndexPageController {
 
-    private final List<AuthUrl> provider;
+    private final Collection<OAuth2ClientProperties.Registration> provider;
 
     @Autowired
     public IndexPageController(OAuth2ClientProperties oAuth2ClientProperties) {
-        this.provider = getAuthUrlsFromProperties(oAuth2ClientProperties);
-    }
-
-    private List<AuthUrl> getAuthUrlsFromProperties(OAuth2ClientProperties oAuth2ClientProperties) {
-        List<AuthUrl> authUrls = new ArrayList<>();
-        var providers = oAuth2ClientProperties.getProvider();
-        oAuth2ClientProperties.getRegistration().values().forEach(registration -> {
-            authUrls.add(new AuthUrl(
-                            registration.getClientName(),
-                            "/oauth2/authorization/" + registration.getProvider()
-                    )
-            );
-        });
-        return authUrls;
+        this.provider = oAuth2ClientProperties.getRegistration().values();
     }
 
 
     @GetMapping("/")
-    public String greeting(Model model, @UserInfo User user) {
+    public String greeting(Model model, @UserInfo User user, HttpServletRequest httpServletRequest) {
+        var path = httpServletRequest.getContextPath();
+        var authUrls = provider.stream().map(registration -> new AuthUrl(
+                registration.getClientName(),
+                path + "/oauth2/authorization/" + registration.getProvider()
+        )).toList();
+
+
         model.addAttribute("user", user);
-        model.addAttribute("authUrls", provider);
+        model.addAttribute("authUrls", authUrls);
         return "index";
     }
 }
